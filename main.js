b4c0652589adb4221f7f0e62f5919e9fc12eaa61
KevGrav <kgravell.prsvr@gmail.com>
2023-10-24 10:21:47 -0400

    with corrections

--- main.js
+++ main.js
@@ -1,16 +1,29 @@
 // ┌─────────────────┐
 // │ Your code here! │	
 // └─────────────────┘
-const ageToAbilities = (age) => {
-    if (age < 16) {
-      return 'You can\'t drive.';
-    }else if (16 <= age <= 17){
-      return 'You can drive but not vote.';
-    }else if (18 <= age <= 24){
-      return 'You can vote but not rent a car.';
-    }else if (age >= 25){
-      return 'You can do pretty much anything.';
-    }
+// const ageToAbilities = (age) => {
+//     if (age < 16) {
+//       return 'You can\'t drive.';
+//     }else if (16 <= age <= 17){
+//       return 'You can drive but not vote.';
+//     }else if (18 <= age <= 24){
+//       return 'You can vote but not rent a car.';
+//     }else if (age >= 25){
+//       return 'You can do pretty much anything.';
+//     }
+// }
+function ageToAbilities(age){
+  switch(true){
+      case age < 16: 
+          return "You can't drive."
+      case age <= 17 && age >= 16:
+          return "You can drive but not vote."
+      case age <= 24 && age >= 18:
+          return "You can vote but not rent a car."
+      case age >= 25:
+          return "You can do pretty much anything."
+      default: break;
+  }
 }
 
 const oddIndices = (arr) => {
@@ -34,7 +47,10 @@ const numOddValues = (arr) => {
 }
 
 const averageStringLength = (arr) => {
-    let result = 0;
+  if(arr.length === 0){
+    return 0
+  }  
+  let result = 0;
     for (let i = 0; i < arr.length; i++){
       result += arr[i].length;
     }
@@ -45,37 +61,63 @@ const firstPunctuationIndex = (str) => {
     let result = 0;
     for (let i = 0; i < str.length; i++){
       if (str[i] === '.' || str[i] === '?' || str[i] === '!'){
-        result = i;
-        break;
-      }
+        return i;
     }
-    return result;
+    }
+    return -1;
 }
 
-const getPlaces = (arr, num) => {
-    let result = [];
-    for (let i = 0; i < arr.length; i++){
-      if (arr[i].length < num){
-        result.push(arr[i]);
-      }
+// const getPlaces = (arr, num) => {
+//     let result = [];
+//     for (let i = 0; i < arr.length; i++){
+//       if (arr[i].length < num){
+//         result.push(arr[i]);
+//       }
 
+//     }
+//     return result;
+// }
+function getPlace(arr, newScore){
+  let place = (arr.length + 1).toString()
+  for(let i = 0; i < arr.length; i++){
+    if(newScore > arr[i]){
+      place = (i + 1).toString()
+      break;
     }
-    return result;
+  }
+  if(place[place.length - 1] === '1'){
+    return place + 'st place'
+  }else if(place[place.length - 1] === '2'){
+    return place + 'nd place'
+  }else if(place[place.length - 1] === '3'){
+    return place + 'rd place'
+  }else{
+    return place + 'th place'
+  }
 }
-    
+
+
 const addToObj = (obj, key, value) => {
-    obj[key] = value;
-    return obj;
+  if(typeof key !== 'string'){
+    return 'Function must be called with a valid key.'
+  }  
+  obj[key] = value;
 }
         
 const duplicateElements = (arr) => {  
-    let result = [];
+  let obj = {}  
+  let result = [];
     for (let i = 0; i < arr.length; i++){
-        if(arr.indexOf(arr[i]) !== arr.lastIndexOf(arr[i])){
-            result.push(arr[i]);
-        }
+      if(obj[arr[i]] === undefined){
+        obj[arr[i]] = 1
+      }else if(obj[arr[i]] === 1){
+        obj[arr[i]] += 1
+        result.push(arr[i])
+      }else{
+        continue;
+      }
     }
-    return result;
+    return result
 }
     
 
